c97e795591e45c516d9211c2f94e8315f1721ebc
thisispriyanshu <priyanshuagrawal801@gmail.com>
2023-04-22 01:41:44 +0530

    sponsor and location page done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Sponsor from "./Screens/Sponsor";
+import Location from "./Screens/Location";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    {/* <Sponsor/> */}
+    <Location/>
+  </div>;
 }
 
 export default App;
