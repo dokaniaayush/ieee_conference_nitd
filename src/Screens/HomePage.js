efee6567b812b057d9cd4db47aab259937b63813
Manan Jain <manan.nitdl@gmail.com>
2023-04-22 02:21:48 +0530

    updates

--- src/Screens/HomePage.js
+++ src/Screens/HomePage.js
@@ -8,15 +8,7 @@ import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div
-      style={{
-        backgroundImage: "url(/img/sample.jpg)",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundAttachment: "fixed",
-        height: "100vh",
-      }}
-    >
+    <div>
       <Navb />
       <div
         style={{
@@ -25,6 +17,10 @@ const HomePage = () => {
           height: "100vh",
           alignItems: "center",
           justifyContent: "center",
+          backgroundImage: "url(/img/sample.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
         }}
       >
         <h1
@@ -41,23 +37,22 @@ const HomePage = () => {
         >
           Conference 2035
         </h1>
-      </div>
-      <br />
-      <div className="socialIconsBar">
-        <ul>
-          <li className="socialIcons">
-            <AiFillFacebook />
-          </li>
-          <li className="socialIcons">
-            <AiFillTwitterCircle />
-          </li>
-          <li className="socialIcons">
-            <AiOutlineInstagram />
-          </li>
-          <li className="socialIcons">
-            <AiFillLinkedin />
-          </li>
-        </ul>
+        <div className="socialIconsBar">
+          <ul>
+            <li className="socialIcons">
+              <AiFillFacebook />
+            </li>
+            <li className="socialIcons">
+              <AiFillTwitterCircle />
+            </li>
+            <li className="socialIcons">
+              <AiOutlineInstagram />
+            </li>
+            <li className="socialIcons">
+              <AiFillLinkedin />
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
