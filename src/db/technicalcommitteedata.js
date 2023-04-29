617575051a6ec1502a501100d11fd484ff37be57
thisispriyanshu <priyanshuagrawal801@gmail.com>
2023-04-30 01:14:21 +0530

    adding some database

---
+++ src/db/technicalcommitteedata.js
@@ -0,0 +1,496 @@
+const TechnicalData = [
+  {
+    id: 1,
+    name: "Prof. Guo-En Chang",
+    description:
+      "Associate Dean, College of Engineering, Chair, Division of Education and Training, AIM-HI, Professor, Department of Mechanical Engineering, Professor, Graduate Institute of Opto-Mechatronics, National Chung-Cheng University.",
+    email: "imegec@ccu.edu.tw",
+  },
+  {
+    id: 2,
+    name: "Prof. Judy Rorison",
+    description:
+      "Opto-electronics expert and professor in the Department of Electrical and Electronics Engineering at the University of Bristol in the United Kingdom.",
+    email: "Judy.Rorison@bristol.ac.uk",
+  },
+  {
+    id: 3,
+    name: "Dr. R. H. Laskar",
+    description:
+      "Expert in electronics and communications engineering and a faculty member in the Department of Electronics and Communication Engineering at NIT Silchar.",
+    email: "rhlaskar@ece.nits.ac.in",
+  },
+  {
+    id: 4,
+    name: "Prof. Rashmi Gupta",
+    description:
+      "Professor of Electronics and Communication Engineering at NSUT East Campus in Delhi.",
+    email: "rashmig71@yahoo.com",
+  },
+  {
+    id: 5,
+    name: "Prof. Munho Kim",
+    description:
+      "Expert in electrical and electronic engineering and a professor at the School of Electrical and Electronic Engineering at Nanyang Technological University in Singapore.",
+    email: "munho.kim@ntu.edu.sg",
+  },
+  {
+    id: 6,
+    name: "Dr. Anirban Bandyopadhyay",
+    description:
+      "Senior Director of Strategic Applications at GLOBALFOUNDRIES, Inc. in Santa Clara, California, USA. An IEEE Distinguished Lecturer.",
+    email: "Anirban_bandy0@ieee.org",
+  },
+  {
+    id: 7,
+    name: "Dr. Amitesh Kumar",
+    description:
+      "Faculty member in the Department of Electronics and Electrical Engineering at Indian Institute of Technology (ISM) Dhanbad.",
+    email: "amitesh@iitism.ac.in",
+  },
+  {
+    id: 8,
+    name: "Prof. John Dallesasse",
+    description:
+      "Faculty member in the Department of Electrical and Computer Engineering at the University of Illinois, Urbana-Champaign, USA.",
+    email: "jdallesa@illinois.edu",
+  },
+  {
+    id: 9,
+    name: "Prof. Richa Bhatia",
+    description:
+      "Professor of Electronics and Communication Engineering at NSUT East Campus in Delhi.",
+    email: "richatimes@gmail.com",
+  },
+  {
+    id: 10,
+    name: "Prof. Manju Khari",
+    description:
+      "Faculty member in the Department of Computer Science Engineering at JNU, Delhi.",
+    email: "manjukhari@mail.jnu.ac.in",
+  },
+  {
+    id: 11,
+    name: "Prof. Bijay K Das",
+    description:
+      "Faculty member in the Department of Electrical Engineering at Indian Institute of Technology Madras.",
+    email: "bkdas@ee.iitm.ac.in",
+  },
+  {
+    id: 12,
+    name: "Dr. Garima Srivastava",
+    description:
+      "Department of Electronics and Communication Engineering, NSUT East Campus, Delhi",
+    email: "garima.shrivastav@gmail.com",
+  },
+  {
+    id: 13,
+    name: "Dr. Rohit Kumar",
+    description:
+      "Department of Electronics and Electrical Engineering, DTU, Delhi",
+    email: "rohit.kumar@dce.ac.in",
+  },
+  {
+    id: 14,
+    name: "Dr. Arun Kumar",
+    description:
+      "Department of Electronics and Communication Engineering, NIT Silchar",
+    email: "arunkumar@ece.nits.ac.in",
+  },
+  {
+    id: 15,
+    name: "Prof. Geeta Sikka",
+    description: "Professor and Head, Department of CSE, NIT Delhi",
+    email: "hodcs@nitdelhi.ac.in",
+  },
+  {
+    id: 16,
+    name: "Prof. Samit K Ray",
+    description:
+      "Department of Physics, Indian Institute of Technology Khargapur",
+    email: "physkr@phy.iitkgp.ac.in",
+  },
+  {
+    id: 17,
+    name: "Dr. Deepti Kakkar",
+    description:
+      "Department of Electronics and Communication Engineering, Dr B R Ambedkar National Institute of Technology, Jalandhar",
+    email: "kakkard@nitd.ac.in",
+  },
+  {
+    id: 18,
+    name: "Dr. Arti Jain",
+    description:
+      "Department of Electronics and Communication Engineering, NSUT East Campus, Delhi",
+    email: "rtjain2001@gmail.com",
+  },
+  {
+    id: 19,
+    name: "Dr. Govind Murmu",
+    description:
+      "Department of Electronics and Electrical Engineering, Indian Institute of Technology (ISM) Dhanbad",
+    email: "govind@iitism.ac.in",
+  },
+  {
+    id: 20,
+    name: "Dr. Tripti Goel",
+    description:
+      "Department of Electronics and Communication Engineering, NIT Silchar",
+    email: "triptigoel@ece.nits.ac.in",
+  },
+  {
+    id: 21,
+    name: "Prof. Poornima Mittal",
+    description:
+      "Department of Electronics and Communication Engineering, DTU, Delhi",
+    email: "poornimamittal@dce.ac.in",
+  },
+  {
+    id: 22,
+    name: "Prof. Mukesh Kumar",
+    description:
+      "Department of Electrical Engineering, Indian Institute of Technology Indore",
+    email: "Mukesh.kr@iiti.ac.in",
+  },
+  {
+    id: 23,
+    name: "Prof. Sudeb Dasgupta",
+    description:
+      "Department of Electronics and Communication Engineering, Indian Institute of Technology Roorkee",
+    email: "sudeb.dasgupta@ece.iitr.ac.in",
+  },
+  {
+    id: 24,
+    name: "Prof. Tarun Kanti Bhattacharyya",
+    email: "tkb@ece.iitkgp.ac.in",
+    description:
+      "Professor and Head, Advanced Technology Development Centre, Department of Electronics and Electrical Communication Engineering, Indian Institute of Technology Khargapur.",
+  },
+  {
+    id: 25,
+    name: "Dr. Amita Jain",
+    email: "amita_jain_17@yahoo.com",
+    description:
+      "Department of Computer Science and Engineering, NSUT East Campus, Delhi.",
+  },
+  {
+    id: 26,
+    name: "Dr. S. K. Tripathy",
+    email: "sktripathy@ece.nits.ac.in",
+    description:
+      "Department of Electronics and Communication Engineering, NIT Silchar.",
+  },
+  {
+    id: 27,
+    name: "Dr. Balwinder Raj",
+    email: "rajb@nitj.ac.in",
+    description:
+      "Department of Electronics and Communication Engineering, Dr B R Ambedkar National Institute of Technology, Jalandhar.",
+  },
+  {
+    id: 28,
+    name: "Prof Neeta Pandey",
+    email: "neetapandey@dce.ac.in",
+    description:
+      "Department of Electronics and Communication Engineering, DTU, Delhi.",
+  },
+  {
+    id: 29,
+    name: "Mr. Preet Yadav",
+    email: "preet.yadav@nxp.com",
+    description:
+      "R & D SOC Technical Program Manager, Automotive Processing, NXP Semiconductors, Noida, India.",
+  },
+  {
+    id: 30,
+    name: "Dr. Neetu Sood",
+    email: "soodn@nitj.ac.in",
+    description:
+      "Department of Electronics and Communication Engineering, Dr B R Ambedkar National Institute of Technology, Jalandhar.",
+  },
+  {
+    id: 31,
+    name: "Dr. Devendra Chack",
+    email: "devendra@iitism.ac.in",
+    description:
+      "Department of Electronics and Electrical Engineering, Indian Institute of Technology (ISM) Dhanbad.",
+  },
+  {
+    id: 32,
+    name: "Dr. Anuradha Chug",
+    email: "anuradha@ipu.ac.in",
+    description:
+      "University School of Information, Communication & Technology, GGSIPU, New Delhi.",
+  },
+  {
+    id: 33,
+    name: "Dr. Ashish Payal",
+    email: "ashish@ipu.ac.in",
+    description:
+      "University School of Information, Communication & Technology, GGSIPU, New Delhi.",
+  },
+  {
+    id: 34,
+    name: "Dr. Rahul Johari",
+    email: "rahul@ipu.ac.in",
+    description:
+      "University School of Automation And Robotics, GGSIPU East campus, New Delhi.",
+  },
+  {
+    id: 35,
+    name: "Prof. Brahamjeet Singh",
+    email: "brahmjit@nitkkr.ac.in",
+    description:
+      "Department of Electronics and Communication Engineering, NIT Kurukshetra.",
+  },
+  {
+    id: 36,
+    name: "Prof. H. M. Gupta",
+    description:
+      "Department of Electronics and Electrical Engineering, IIT Delhi",
+    email: "hmgupta@ee.iitd.ac.in",
+  },
+  {
+    id: 37,
+    name: "Prof. Shaibal Mukherjee",
+    description:
+      "Department of Electrical Engineering (Hybrid Nanodevice Research Group), Indian Institute of Technology Indore",
+    email: "shaibal@iiti.ac.in",
+  },
+  {
+    id: 38,
+    name: "Prof. M. P. S. Bhatia",
+    description:
+      "Department of Computer Science Engineering, NSUT Dwarka, Delhi",
+    email: "mpsbhatia@nsut.ac.in",
+  },
+  {
+    id: 39,
+    name: "Prof. Manoj Gaur",
+    description:
+      "Department of Computer Science Engineering, NSUT East Campus, Delhi",
+    email: "manojgaur@yahoo.com",
+  },
+  {
+    id: 40,
+    name: "Prof. O. P. Sahu",
+    description:
+      "Department of Electronics and Communication Engineering, NIT Kurukshetra",
+    email: "ops_nitk@yahoo.co.in",
+  },
+  {
+    id: 41,
+    name: "Prof. Subhananda Chakraborty",
+    description:
+      "Department of Electrical Engineering, Indian Institute of Technology Bombay",
+    email: "subho@ee.iitb.ac.in",
+  },
+  {
+    id: 42,
+    name: "Prof. Sandeep Arya",
+    description:
+      "Department of Electronics and Communication Engineering, GJUS&T, Hisar, Haryana",
+    email: "arya1sandeep@gmail.com",
+  },
+  {
+    id: 43,
+    name: "Prof. Sandeep Kumar",
+    description:
+      "Department of Electronics and Communication Engineering, PEC Chandigarh",
+    email: "sandeepk@gmail.com",
+  },
+  {
+    id: 44,
+    name: "Dr. Ramesh Kumar Vats",
+    description:
+      "Dept of Mathematics & Scientific Computing, NIT Hamirpur, H.P. , India",
+    email: "rkvats@nith.ac.in",
+  },
+  {
+    id: 45,
+    name: "Prof. Amit Garg",
+    description:
+      "Department of Electronics and Communication Engineering, DCRUST, Murthal, Haryana",
+    email: "garg_amit03@yahoo.co.in",
+  },
+  {
+    id: 46,
+    name: "Prof. Yogesh Chaba",
+    description:
+      "Department of Computer Science & Engineering, GJUS&T, Hisar, Haryana",
+    email: "yogeshchaba@yahoo.com",
+  },
+  {
+    id: 47,
+    name: "Prof. Dinesh Kumar",
+    description:
+      "Department of Electronics and Communication Engineering, DTU, Delhi",
+    email: "dinesh_chutani@yahoo.com",
+  },
+  {
+    id: 48,
+    name: "Prof. Amit Prakash Singh",
+    email: "amit@ipu.ac.in",
+    description:
+      "Professor at University School of Information, Communication & Technology, GGSIPU, New Delhi",
+  },
+  {
+    id: 49,
+    name: "Prof. Anjana Gosain",
+    email: "anjana_gosain@ipu.ac.in",
+    description:
+      "Professor at University School of Information, Communication & Technology, GGSIPU, New Delhi",
+  },
+  {
+    id: 50,
+    name: "Prof. Bharti Suri",
+    email: "bhartisuri@gmail.com",
+    description:
+      "Professor at University School of Information, Communication & Technology, GGSIPU, New Delhi",
+  },
+  {
+    id: 51,
+    name: "Prof. Virendra Prasad Vishwakarma",
+    email: "vpv@ipu.ac.in",
+    description:
+      "Professor at University School of Information, Communication & Technology, GGSIPU, New Delhi",
+  },
+  {
+    id: 52,
+    name: "Prof. Ajay Singholi",
+    email: "ajay.s@ipu.ac.in",
+    description:
+      "Professor at University School of Automation and Robotics, GGSIPU East campus, New Delhi",
+  },
+];
+
+const InternationalData = [
+  {
+    id: 2,
+    name: "Prof. Hiroshi Iwai",
+    description:
+      "Vice Dean and Distinguished Chair Professor\nNational Yang Ming Chiao Tung University, Hsinchu, Taiwan",
+    email: "h.iwai@ieee.org",
+  },
+  {
+    id: 3,
+    name: "Prof. Jamal Deen",
+    description: "ECE Department, McMaster University\nCanada",
+    email: "jamal@mcmaster.ca",
+  },
+  {
+    id: 4,
+    name: "Prof. Jayanta Sarma",
+    description: "University of Bath, UK\njsarmabath@gmail.com",
+    email: "",
+  },
+  {
+    id: 5,
+    name: "Prof. Judy Rorrison",
+    description:
+      "Department of Electrical and Electronic Engineering, University of Bristol, UK\nJudy.Rorison@bristol.ac.uk",
+    email: "",
+  },
+  {
+    id: 6,
+    name: "Prof. Guo-En Chang",
+    description:
+      "College of Engineering and AIM-HI, National Chun Cheng University, Taiwan\nsamar@ieee.org",
+    email: "",
+  },
+  {
+    id: 7,
+    name: "Prof. Richard Hogg",
+    description:
+      "Dept. of Electronic and Nanoscale Engineering, University of Glasgow, UK\nRichard.Hogg@glasgow.ac.uk",
+    email: "",
+  },
+  {
+    id: 8,
+    name: "Dr. Anirban Bandyopadhyay",
+    description:
+      "Senior Director, Strategic Applications, GLOBALFOUNDRIES, Inc., Santa Clara, California, USA\nIEEE Distinguished Lecturer",
+    email: "",
+  },
+  {
+    id: 9,
+    name: "Dr. Martin Lopez-Garcia",
+    description:
+      "Natural and Artificial Photonic Structures and Devices Group, International Iberian Nanotechnology Laboratory, Portugal\nM.LopezGarcia@leeds.ac.uk",
+    email: "",
+  },
+  {
+    id: 10,
+    name: "Prof. Arturas Serackis",
+    description:
+      "Professor & Head Department of Electronics, Vilnius University, Lithuania\narturas@ieee.org, arturas.serackis@vilniustech.lt",
+    email: "",
+  },
+  {
+    id: 11,
+    name: "Dr. Santosh Kumar",
+    description:
+      "Associate Professor\nLiocheng University, China, School of Physics Science and Information Technology, No. 1, Hunan Road, Liaocheng, Shandong 252059, China\nsantosh@icu.edu.cn",
+    email: "",
+  },
+  {
+    id: 12,
+    name: "Dr. Miaoqing Huang",
+    description:
+      "Associate Professor, Computer Science & Computer Engineering Department,University of Arkansas,Fayetteville, AR 72701, USA\nmqhuang@uark.edu",
+    email: "",
+  },
+  {
+    id: 13,
+    name: "Dr. Ghanshyam",
+    email: "ghanshyams@uj.ac.za",
+    description:
+      "Professor, Dept. of Electrical & Electronics Engg. Science, B2, Lab 105, Auckland Park, Kingsway Campus, Univ. of Johannesburg, PO Box 524, Johannesburg 2006 South Africa",
+  },
+  {
+    id: 14,
+    name: "Dr. Seok-Bum Ko",
+    email: "seokbum.ko@usask.ca",
+    description:
+      "Professor, Electrical and Computer Engineering, Biomedical Engineering, University of Saskatchewan, Room 3B31 Engineering Building, 57 Campus Drive,Saskatoon, SK, Canada",
+  },
+  {
+    id: 15,
+    name: "Dr. Eunice Sari",
+    email: "e.sari@ecu.edu.au",
+    description: "Professor, Murdoch University, Australia",
+  },
+  {
+    id: 16,
+    name: "Dr. Ahmed Hemani",
+    email: "hemani@kth.se",
+    description:
+      "School of ICT, KTH – Royal Institute of Technology, Kista, Sweden, Postal address: ESY/ICT/KTH, Electrum 229, SE-164 40, Kista, Sweden, Visiting Address: 3rd Floor, Elevator B, Electrum 229, 20-26, Kista, Sweden",
+  },
+  {
+    id: 17,
+    name: "Dr. Hironori Washizaki",
+    email: "Washizaki@waseda.jp",
+    description: "Professor, Department of CSE, Waseda University, Japan",
+  },
+  {
+    id: 18,
+    name: "Dr. Luiz Fernando Capretz",
+    email: "lcapretz@uwo.ca",
+    description:
+      "Professor, Department of Electrical and Computer Engg., Western University, Canada",
+  },
+  {
+    id: 19,
+    name: "Dr. P K Mahanti",
+    email: "pmahanti@unbsj.ca",
+    description:
+      "Professor, Department of Computer Science and Applied Statics, University of New Brunswick, Canada",
+  },
+  {
+    id: 20,
+    name: "Dr. Narayan Debnath",
+    email: "ndebnath@winona.edu",
+    description: "Professor, Department of CSE, Winona State University, USA",
+  },
+];
