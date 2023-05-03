e9a2015158885448c22350d1a13422aac88422c6
Manan Jain <manan.nitdl@gmail.com>
2023-05-03 16:03:30 +0530

    blah blah

--- src/screens/AboutConference.js
+++ src/screens/AboutConference.js
@@ -26,42 +26,18 @@ const AboutConference = () => {
       >
         <h3>About The Conference</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac
-          condimentum odio, et ornare leo. Sed aliquam, lacus vehicula hendrerit
-          efficitur, justo lorem mattis felis, ut imperdiet nunc est vitae sem.
-          Vestibulum egestas volutpat nisl, vitae elementum justo convallis sit
-          amet. Nullam faucibus tortor nisi, vel aliquet tortor aliquet ut.
-          Quisque porta aliquet velit id scelerisque. Proin ipsum mi,
-          scelerisque eu ante sit amet, facilisis tempor eros. Praesent posuere
-          metus purus, et pulvinar lorem venenatis eget. Nullam at nisi id augue
-          imperdiet vestibulum. Vestibulum quis auctor diam. Vestibulum rhoncus
-          erat sed varius aliquet. Suspendisse luctus mi eu neque maximus
-          lacinia ac at quam. Donec id vehicula purus. Pellentesque habitant
-          morbi tristique senectus et netus et malesuada fames ac turpis
-          egestas. Nullam lobortis sem non lorem tristique, eu laoreet metus
-          viverra. Nulla venenatis urna vel libero auctor mollis. Mauris semper
-          gravida tortor vel consectetur. Donec metus leo, finibus sed nisl ac,
-          viverra iaculis ex. Proin felis nisi, auctor vitae mollis eu,
-          sollicitudin in dolor. Phasellus ipsum nibh, eleifend et leo vel,
-          placerat luctus risus. Praesent at euismod augue. Donec id porta
-          metus, quis euismod odio. In lobortis pharetra tellus, a mollis sem.
-          Nam accumsan urna sed erat mattis, at porta velit blandit. Integer
-          volutpat ex nec tincidunt tempus. Morbi commodo imperdiet justo. In
-          rhoncus feugiat aliquam. Nunc vehicula maximus elit. Cras eu ex non
-          elit efficitur sodales. Vivamus sed arcu at leo pulvinar auctor.
-          Integer lacinia, odio quis laoreet pharetra, nisi libero luctus
-          turpis, eu aliquet turpis eros vel sapien. Maecenas nulla augue,
-          condimentum id urna non, efficitur tempor turpis. Ut id sapien sapien.
-          Integer aliquet fringilla mollis. In blandit tristique tellus, non
-          tempus sapien condimentum a. Interdum et malesuada fames ac ante ipsum
-          primis in faucibus. Praesent sagittis sem neque, a semper augue
-          volutpat quis. Vestibulum sed vulputate arcu, ut ullamcorper mi.
-          Vivamus lacinia mauris finibus sapien elementum vulputate. Fusce nec
-          dui eget magna bibendum gravida. Quisque hendrerit varius velit quis
-          venenatis. Pellentesque semper, nisl et congue sodales, libero felis
-          vestibulum lacus, vel rutrum turpis neque vel quam. Lorem ipsum dolor
-          sit amet, consectetur adipiscing elit. Mauris id eros turpis. Maecenas
-          a interdum mi. Mauris scelerisque risus hendrerit, facilisis odio nec,
+          The conference is being proposed to be organized for the first time
+          with the theme of “Microelectronics, Communication and Signal
+          Processing.” However, ICMCSP 2024 solicits original papers’
+          contributions in all related areas. The theme of the conference is
+          “Microelectronics Communication and Signal Processing.” However,
+          ICMCSP 2024 solicits original papers’ contributions in all related
+          areas. The main but not limited tracks of the conference are focused
+          on Microelectronic Circuits and Systems, Communication Systems &
+          Networks, Signal Processing and Applications, and VLSI Technology and
+          Embedded Systems. The committee may propose to publish the accepted
+          and selected then after papers, only after the due presentation and
+          evaluation, through the IEEE Xplore.
         </p>
       </div>
     </div>
--- src/screens/AboutInstitute.js
+++ src/screens/AboutInstitute.js
@@ -23,42 +23,42 @@ const AboutInstitute = () => {
           textAlign: "justify",
         }}
       >
-        <h3>About The Institute</h3>
+        <h3>About The Department</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac
-          condimentum odio, et ornare leo. Sed aliquam, lacus vehicula hendrerit
-          efficitur, justo lorem mattis felis, ut imperdiet nunc est vitae sem.
-          Vestibulum egestas volutpat nisl, vitae elementum justo convallis sit
-          amet. Nullam faucibus tortor nisi, vel aliquet tortor aliquet ut.
-          Quisque porta aliquet velit id scelerisque. Proin ipsum mi,
-          scelerisque eu ante sit amet, facilisis tempor eros. Praesent posuere
-          metus purus, et pulvinar lorem venenatis eget. Nullam at nisi id augue
-          imperdiet vestibulum. Vestibulum quis auctor diam. Vestibulum rhoncus
-          erat sed varius aliquet. Suspendisse luctus mi eu neque maximus
-          lacinia ac at quam. Donec id vehicula purus. Pellentesque habitant
-          morbi tristique senectus et netus et malesuada fames ac turpis
-          egestas. Nullam lobortis sem non lorem tristique, eu laoreet metus
-          viverra. Nulla venenatis urna vel libero auctor mollis. Mauris semper
-          gravida tortor vel consectetur. Donec metus leo, finibus sed nisl ac,
-          viverra iaculis ex. Proin felis nisi, auctor vitae mollis eu,
-          sollicitudin in dolor. Phasellus ipsum nibh, eleifend et leo vel,
-          placerat luctus risus. Praesent at euismod augue. Donec id porta
-          metus, quis euismod odio. In lobortis pharetra tellus, a mollis sem.
-          Nam accumsan urna sed erat mattis, at porta velit blandit. Integer
-          volutpat ex nec tincidunt tempus. Morbi commodo imperdiet justo. In
-          rhoncus feugiat aliquam. Nunc vehicula maximus elit. Cras eu ex non
-          elit efficitur sodales. Vivamus sed arcu at leo pulvinar auctor.
-          Integer lacinia, odio quis laoreet pharetra, nisi libero luctus
-          turpis, eu aliquet turpis eros vel sapien. Maecenas nulla augue,
-          condimentum id urna non, efficitur tempor turpis. Ut id sapien sapien.
-          Integer aliquet fringilla mollis. In blandit tristique tellus, non
-          tempus sapien condimentum a. Interdum et malesuada fames ac ante ipsum
-          primis in faucibus. Praesent sagittis sem neque, a semper augue
-          volutpat quis. Vestibulum sed vulputate arcu, ut ullamcorper mi.
-          Vivamus lacinia mauris finibus sapien elementum vulputate. Fusce nec
-          dui eget magna bibendum gravida. Quisque hendrerit varius velit quis
-          venenatis. Pellentesque semper, nisl et congue sodales, libero felis
-          vestibulum lacus, vel rutrum turpis neque vel quam. Lorem ipsum dolor
+          The Department of Electronic and Communication Engineering (ECE),
+          National Institute of Technology Delhi was established in 2010,
+          immediately with the beginning of the Institute under the aegis of the
+          Ministry of Human Resource and Development (MHRD), Govt. of India.
+          Currently, it is offering one Undergraduate (B. Tech.) Program and two
+          Postgraduate (M. Tech) programs in ECE and VLSI Design. The Department
+          also offers Ph.D. and Post-Doctoral Fellowship (PDF) Programme in
+          relevant areas, like electronic devices and circuits, semiconductor
+          photonics, optical communication and opto-electronics, signal & speech
+          processing, analog & digital VLSI design and circuits, antenna & wave
+          propagation, networking, AI and ML etc. It has excellent laboratories
+          and research facilities in electronic devices and circuits, electronic
+          measurement and instrumentation, microprocessor and microcontroller,
+          microwave and antenna design, optical fiber communication and optical
+          device, multimedia, and advanced communication and design automation
+          and simulation laboratory. The faculty of the department has published
+          123 reputed international journal papers and 78 international/
+          national conference papers in the last 3 academic years. Departmental
+          faculty has published journal papers in IEEE Transaction on Electronic
+          devices, IEEE Photonics Technology, IEEE Electron Devices, Springer
+          journals, Elsevier journals etc. The Department and faculty members
+          have received projects, grants, and fellowships from the Ministry of
+          Electronics and Information Technology (MeitY), the Department of
+          Science and Technology (DST)-SERB, and other funding agencies. The
+          Department has active collaborations with academic Institutes &
+          research institutes in India and abroad. The Department of ECE has a
+          blend of young as well as experienced dynamic faculty members and is
+          committed to providing quality education and research in the field.
+          Faculty members of the department have excellent academic & research
+          credentials and published numerous articles in the diversified field
+          and have adequate experience in advanced research. The department of
+          ECE provides a creative learning environment to the students for
+          excellence in technical education. The department hopes to achieve the
+          national goals and objectives of industrialization and self-reliance.
         </p>
       </div>
     </div>
