28c1feb10839d6ab05269dc6adb29cc36fff160f
unknown <ayushdokania007@gmail.com>
2023-05-17 11:21:00 +0530

    heading color updated

--- src/screens/AboutConference.js
+++ src/screens/AboutConference.js
@@ -15,34 +15,35 @@ const AboutConference = () => {
     >
       <div
         style={{
-          width: "80%",
+          width: "60%",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
           marginTop: "20px",
-          textAlign: "justify",
         }}
       >
-        <h3>About The Conference</h3>
-        <p>
-          International Conference on Microelectronics, Communication and Signal
-          Processing (ICMCSP 2024), provides a platform for researchers,
-          academics, scholars, industry experts from India and the world to
-          present and discuss their research findings in various areas of
-          Electronics and Communication Engineering. However, ICMCSP 2024
-          solicits original papers’ contributions in all related areas of
-          Electronics Engineering.
-        </p>
-
-        <p>
-          The major tracks of the conference are focused on Microelectronic
-          Circuits and Systems, Communication Systems & Networks, Signal
-          Processing & Applications, and VLSI Technology & Embedded Systems. The
-          conference will feature keynote speakers, technical sessions, and
-          workshops covering a wide range of topics related to Microelectronics,
-          VLSI Design, Signal Processing and Communication systems.
-        </p>
+        <h3 style={{color: "#273053"}}>About The Conference</h3>
+        <br/>
+        <div style={{textAlign: "justify",}}>
+          <p>
+            International Conference on Microelectronics, Communication and Signal
+            Processing (ICMCSP 2024), provides a platform for researchers,
+            academics, scholars, industry experts from India and the world to
+            present and discuss their research findings in various areas of
+            Electronics and Communication Engineering. However, ICMCSP 2024
+            solicits original papers’ contributions in all related areas of
+            Electronics Engineering.
+          </p>
+          <p>
+            The major tracks of the conference are focused on Microelectronic
+            Circuits and Systems, Communication Systems & Networks, Signal
+            Processing & Applications, and VLSI Technology & Embedded Systems. The
+            conference will feature keynote speakers, technical sessions, and
+            workshops covering a wide range of topics related to Microelectronics,
+            VLSI Design, Signal Processing and Communication systems.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/screens/AboutInstitute.js
+++ src/screens/AboutInstitute.js
@@ -14,46 +14,48 @@ const AboutInstitute = () => {
     >
       <div
         style={{
-          width: "80%",
+          width: "60%",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
           marginTop: "20px",
-          textAlign: "justify",
         }}
       >
-        <h3>
+        <h3 style={{color: "#273053"}}>
           About the Department of Electronics and Communication Engineering, NIT
           Delhi
         </h3>
-        <p>
-          The Department of ECE was established in 2010, immediately with the
-          beginning of the Institute under the aegis of the Ministry of
-          Education (MoE), Govt. of India. Currently, the Department is offering
-          one undergraduate program as B. Tech (ECE) and two postgraduate
-          programs as M. Tech. ECE and M. Tech. ECE (VLSI). The Department also
-          offers Ph. D. and Post-Doctoral Fellowship (PDF) Programme in relevant
-          areas of electronics and communication engineering. Department has
-          good infrastructure including laboratories and research facilities in
-          electronic devices and circuits, electronic measurement and
-          instrumentation, microprocessor and microcontroller, microwave and
-          antenna design, optical fiber communication and optical devices,
-          multimedia, advanced communication and VLSI design. Faculty members of
-          ECE department have received projects, grants, and fellowships from
-          various funding agencies. The Department has active collaborations
-          with academic & research institutes.
-        </p>
-        <p>
-          The Department of ECE has a blend of young as well as experienced
-          dynamic faculty members and is committed to providing quality
-          education and research in the field. Faculty members of the department
-          have excellent academic & research credentials and published many
-          peer-reviewed journal articles/papers, Books, Book Chapters, etc. in
-          the diversified field and have adequate experience in teaching and
-          research. The department of ECE provides a creative learning
-          environment to the students for excellence in technical education.
-        </p>
+        <br/>
+        <div style={{textAlign: "justify",}}>
+          <p>
+            The Department of ECE was established in 2010, immediately with the
+            beginning of the Institute under the aegis of the Ministry of
+            Education (MoE), Govt. of India. Currently, the Department is offering
+            one undergraduate program as B. Tech (ECE) and two postgraduate
+            programs as M. Tech. ECE and M. Tech. ECE (VLSI). The Department also
+            offers Ph. D. and Post-Doctoral Fellowship (PDF) Programme in relevant
+            areas of electronics and communication engineering. Department has
+            good infrastructure including laboratories and research facilities in
+            electronic devices and circuits, electronic measurement and
+            instrumentation, microprocessor and microcontroller, microwave and
+            antenna design, optical fiber communication and optical devices,
+            multimedia, advanced communication and VLSI design. Faculty members of
+            ECE department have received projects, grants, and fellowships from
+            various funding agencies. The Department has active collaborations
+            with academic & research institutes.
+          </p>
+          <p>
+            The Department of ECE has a blend of young as well as experienced
+            dynamic faculty members and is committed to providing quality
+            education and research in the field. Faculty members of the department
+            have excellent academic & research credentials and published many
+            peer-reviewed journal articles/papers, Books, Book Chapters, etc. in
+            the diversified field and have adequate experience in teaching and
+            research. The department of ECE provides a creative learning
+            environment to the students for excellence in technical education.
+          </p>
+        </div>
       </div>
     </div>
   );
